0ea45599f4b8ebd2e6617134d2d1b586104daaa1
Pro100Im <pro100elite@gmail.com>
2025-03-12 22:01:39 +0100

    save

--- Assets/Scripts/Game/Pause.cs
+++ Assets/Scripts/Game/Pause.cs
@@ -1,7 +1,5 @@
-using DG.Tweening;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
 namespace Game
 {
@@ -9,14 +7,11 @@ namespace Game
     {
         [SerializeField] private float fadeDuration = 0.3f;
         [Space]
-        [SerializeField] private CanvasGroup _canvasGroup;
-        [SerializeField] private Button backToMenuBtb;
+        [SerializeField] private GameObject panel;
 
         private bool _isPaused = false;
 
         private TankInput _input;
-
-        private void Awake() => backToMenuBtb.onClick.AddListener(BackToMenu);
 
         public void Init(TankInput input)
         {
@@ -29,42 +24,22 @@ namespace Game
         {
             if(!_isPaused)
             {
-                //Time.timeScale = 0;
+                Time.timeScale = 0;
 
-                _canvasGroup.gameObject.SetActive(true);
+                panel.SetActive(true);
 
                 _isPaused = true;
             }
             else
             {
-                //Time.timeScale = 1;
+                Time.timeScale = 1;
 
-                _canvasGroup.gameObject.SetActive(false);
+                panel.SetActive(false);
 
                 _isPaused = false;
             }
         }
 
-        //private void FadePanel(float endValue)
-        //{
-        //    _canvasGroup.DOFade(endValue, fadeDuration).SetUpdate(false);
-        //    _canvasGroup.blocksRaycasts = endValue > 0;
-        //}
-
-        private void BackToMenu()
-        {
-            Debug.Log("Button Clicked!");
-
-            //Time.timeScale = 1;
-
-            SceneLoader.Instance.LoadMenuScene();
-        }
-
-        private void OnDestroy()
-        {
-            _input.ActionMap.Pause.canceled -= PauseGame;
-
-            backToMenuBtb.onClick.RemoveListener(BackToMenu);
-        }
+        private void OnDestroy() => _input.ActionMap.Pause.canceled -= PauseGame;
     }
 }
